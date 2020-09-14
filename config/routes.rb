ac59642503d54bc8a6cae1eda8eb2139f08b4577
zsteen13 <zsteen13@gmail.com>
2020-09-14 01:15:32 -0500

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get 'welcome', to: 'sessions#welcome'
   get 'authorized', to: 'sessions#page_requires_login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome'
+  root 'sessions#welcome'
 end
