d698d8a717177451cbf20a6a28c1ad8f137aec89
zsteen13 <zsteen13@gmail.com>
2020-09-13 22:15:26 -0500

    added user system

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,4 @@ Rails.application.routes.draw do
   get 'welcome', to: 'sessions#welcome'
   get 'authorized', to: 'sessions#page_requires_login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'sessions#welcome'
 end
