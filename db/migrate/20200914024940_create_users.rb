724a9f00a1f2c74b1cf17682c0ee3ee9d4305048
[full name (new)] <zac@DESKTOP-0PP4FL9.localdomain>
2020-09-16 18:07:27 -0500

    database testing

--- db/migrate/20200914024940_create_users.rb
+++ db/migrate/20200914024940_create_users.rb
@@ -1,20 +1,18 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :password_digest
 
       t.integer :uin
-      t.string : first_name
-      t.string: last_name
-      t.string: email
-      t.string committee
-      t.string subcommittee
-      t.integer total_points
-      t.integer meeting_points
-      t.integer event_points
-      t.integer misc_points
-      t.boolean admin
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :committee
+      t.string :subcommittee
+      t.integer :total_points
+      t.integer :meeting_points
+      t.integer :event_points
+      t.integer :misc_points
+      t.boolean :admin
         
       end
 
