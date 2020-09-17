8170e37cdf8d76074737a0186a201b3ad1525bf5
[full name (new)] <zac@DESKTOP-0PP4FL9.localdomain>
2020-09-16 20:53:01 -0500

    test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(last_name: params[:last_name])
     if @user
-      sessions[:user_id] = @user.id
+      session[:user_id] = @user.id
       redirect_to '/welcome'
     else
       redirect_to '/login'
