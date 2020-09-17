de679931593e940af7ec90771e985ceb8c8a810e
[full name (new)] <zac@DESKTOP-0PP4FL9.localdomain>
2020-09-16 19:37:59 -0500

    users table update

--- db/migrate/20200917003720_create_users.rb
+++ db/migrate/20200917003720_create_users.rb
@@ -1,20 +1,17 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-
-      t.integer :uin
+      t.int :uin
       t.string :first_name
       t.string :last_name
       t.string :email
       t.string :committee
       t.string :subcommittee
-      t.integer :total_points
-      t.integer :meeting_points
-      t.integer :event_points
-      t.integer :misc_points
+      t.int :total_points
+      t.int :meeting_points
+      t.int :event_points
+      t.int :misc_points
       t.boolean :admin
-        
-      end
 
       t.timestamps
     end
