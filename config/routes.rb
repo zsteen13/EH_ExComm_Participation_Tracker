e06dcb41606eee6f3e8e59d72c96a90e40fc47c5
[full name (new)] <zac@DESKTOP-0PP4FL9.localdomain>
2020-09-16 22:47:41 -0500

    added activities

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get 'authorized', to: 'sessions#page_requires_login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'sessions#welcome'
-  get 'profile', to: 'users#profile'
-
+  get 'profile', to: 'profile#profile'
+  get 'activities', to: 'activities#activities'
 end
