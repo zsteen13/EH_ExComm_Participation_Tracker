fbfe8a1db07c47f1cf8ce1ad90a5962c5a6ba0c0
Trevor Moore <moore.trev@tamu.edu>
2020-09-17 17:43:10 -0500

    working on adding create activities

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+
+  get 'activities/index'
+  get 'activities/create'
   resources :users, only: [:new, :create]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
   get 'authorized', to: 'sessions#page_requires_login'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'sessions#welcome'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_024940) do
+ActiveRecord::Schema.define(version: 2020_09_16_010054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.decimal "point_value"
+    t.string "description"
+    t.decimal "num_rsvp"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
