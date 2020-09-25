fa7dd8bad19447a4ce5e2180b2f2fc539ceef0b9
Matthew Coming <coming.matthew@gmail.com>
2020-09-25 02:50:55 -0500

    bug fixes, style changes for uniformity

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,24 @@ class User < ApplicationRecord
     e = e.strip if e
     super(e)
   end
+
+  def display_total_points
+    self[:total_points] || "0"
+  end
+
+  def display_meeting_points
+    self[:meeting_points] || "0"
+  end
+
+  def display_event_points
+    self[:event_points] || "0"
+  end
+
+  def display_committee
+    self[:committee].blank? ? "No assigned committee" : self[:committee]
+  end
+
+  def display_subcommittee
+    self[:subcommittee].blank? ? "No assigned committee" : self[:subcommittee]
+  end
 end
