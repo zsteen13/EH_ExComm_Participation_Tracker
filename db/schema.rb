31d06753c38a435f740ca1c6a5b6e229c1ebb243
zsteen13 <zsteen13@tamu.edu>
2020-10-02 17:56:13 -0500

    added simple cov and rspec

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_09_18_040526) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "activities", force: :cascade do |t|
     t.string "name"
     t.string "_type"
