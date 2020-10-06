4e49ffb6a5287778d8f92a49d1d02eaf2725f58f
Trevor Moore <moore.trev@tamu.edu>
2020-10-06 08:10:59 -0500

    constants were defined properly

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails'
+  gem 'capybara', '>= 2.15'
+
 end
 
 group :development do
@@ -50,7 +52,6 @@ end
 
 group :test do
   # Adds support for Capybara system testing and selenium driver
-  gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
--- app/helpers/bulk_add_users_helper.rb
+++ app/helpers/bulk_add_users_helper.rb
@@ -1,75 +1,74 @@
-require 'csv'
+require "csv"
 
 module BulkAddUsersHelper
-    def BulkAddUsersHelper.createFile(uploaded_file, filename)
-        File.open(filename, 'wb') do |file|
-            file.write(uploaded_file.read)
-          end
+  def BulkAddUsersHelper.createFile(uploaded_file, filename)
+    File.open(filename, "wb") do |file|
+      file.write(uploaded_file.read)
+    end
+  end
+
+  UIN_COL_CONST = 0
+  FIRST_NAME_COL_CONST = 1
+  LAST_NAME_CO_CONST = 2
+  EMAIL_COL_CONST = 3
+  COMMITTEE_COL_CONST = 4
+  SUBCOMMITTE_COL_CONST = 5
+  TOTAL_POINTS_COL_CONST = 6
+  MEETING_POINTS_COL_CONST = 7
+  EVENT_POINTS_COL_CONST = 8
+  MISC_POINTS_COL_CONST = 9
+  ADMIN_COL_CONST = 10
+
+  def BulkAddUsersHelper.parseData(filename)
+    csv = CSV.read(filename)
+    users = []
+
+    valid = true
+
+    upperBound = csv.count - 1
+    lowerBound = 1 # loses the title info
+    for i in lowerBound..upperBound
+      user = User.new(
+        uin: csv[i][UIN_COL],
+        first_name: csv[i][FIRST_NAME_COL_CONST],
+        last_name: csv[i][LAST_NAME_COL_CONST],
+        email: csv[i][EMAIL_COL_CONST],
+        committee: csv[i][COMMITTEE_COL_CONST],
+        subcommittee: csv[i][SUBCOMMITTE_COL_CONST],
+        total_points: csv[i][TOTAL_POINTS_COL_CONST],
+        meeting_points: csv[i][MEETING_POINTS_COL_CONST],
+        event_points: csv[i][EVENT_POINTS_COL_CONST],
+        misc_points: csv[i][MISC_POINTS_COL_CONST],
+        admin: csv[i][ADMIN_COL_CONST],
+      )
+
+      users.append(user)
+
+      if user.invalid?
+        valid = false
+      end
     end
 
-    def BulkAddUsersHelper.parseData(filename)
-        UIN_COL = 0
-        FIRST_NAME_COL = 1
-        LAST_NAME_COL = 2
-        EMAIL_COL = 3
-        COMMITTEE_COL = 4
-        SUBCOMMITTE_COL = 5
-        TOTAL_POINTS_COL = 6
-        MEETING_POINTS_COL = 7 
-        EVENT_POINTS_COL = 8 
-        MISC_POINTS_COL = 9 
-        ADMIN_COL = 10
-        
-        csv = CSV.read(filename)        
-        users = []
+    return users, valid
+  end
 
-        valid = true
+  def BulkAddUsersHelper.checkNumColumns(filename, numCols = 11)
+    csv = CSV.read(filename)
 
-        upperBound = csv.count - 1
-        lowerBound = 1 # loses the title info
-        for i in lowerBound..upperBound
-            user = User.new(
-                uin:csv[i][UIN_COL],
-                first_name:csv[i][FIRST_NAME_COL],
-                last_name:csv[i][LAST_NAME_COL],
-                email:csv[i][EMAIL_COL],
-                committee:csv[i][COMMITTEE_COL],
-                subcommittee:csv[i][subcommittee_col],
-                total_points:csv[i][TOTAL_POINTS_COL],
-                meeting_points:csv[i][MEETING_POINTS_COL],
-                event_points:csv[i][EVENT_POINTS_COL],
-                misc_points:csv[i][MISC_POINTS_COL],
-                admin:csv[i][ADMIN_COL]
-            )
-
-            users.append(user)
-
-            if user.invalid?
-                valid = false
-            end
-        end
-
-        return users, valid
-
+    upperBound = csv.count - 1
+    retval = true
+    for i in 0..upperBound
+      if csv[i].count != numCols
+        return false, i, csv[i].count
+      end
     end
 
-    def BulkAddUsersHelper.checkNumColumns(filename, numCols = 11)
-        csv = CSV.read(filename)        
+    return retval, 0, 0
+  end
 
-        upperBound = csv.count - 1
-        retval = true
-        for i in 0..upperBound
-            if csv[i].count != numCols
-                return false, i, csv[i].count 
-            end
-        end
-
-        return retval, 0, 0
+  def BulkAddUsersHelper.saveUsers(users)
+    users.each do |user|
+      user.save()
     end
-    
-    def BulkAddUsersHelper.saveUsers(users)
-        users.each do |user|
-            user.save()
-        end
-    end
+  end
 end
