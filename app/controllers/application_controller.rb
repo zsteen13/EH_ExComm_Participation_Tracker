ecaa74a39b732990766ae71c46f02f5eacb455cc
zsteen13 <zac@DESKTOP-0PP4FL9.localdomain>
2020-10-09 01:02:30 -0500

    disable CSRF token for mobile support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+    protect_from_forgery unless: -> { request.format.json? }
+
     before_action :authorized
     helper_method :current_user
     helper_method :logged_in?
