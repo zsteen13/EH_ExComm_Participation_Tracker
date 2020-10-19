9d812b16aacc9d4c8e5cb9d5ae9247a5b5e1cf10
Trevor Moore <moore.trev@tamu.edu>
2020-10-18 21:52:54 -0500

    not sure how to this working

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,24 +2,22 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
-# Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
-# Coverage report generator
 require 'simplecov'
 SimpleCov.start
-# filter out support specs, tests
 SimpleCov.add_filter 'spec/support'
-
 require 'capybara/rails'
-
 require 'rails/test_help'
+
+require 'selenium-webdriver'
+
 
 Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome, executable_path: "/usr/bin/chromedriver")
 end
 
+Capybara.javascript_driver = :selenium
 Capybara.javascript_driver = :selenium
 
 Rails.application.load_seed
