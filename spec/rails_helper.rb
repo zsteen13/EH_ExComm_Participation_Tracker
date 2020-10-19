bc0d6a3543ae158bfeb8729d46cec1176aafa461
Trevor Moore <moore.trev@tamu.edu>
2020-10-19 13:00:04 -0500

    changed2

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,6 +14,7 @@ require "selenium-webdriver"
 
 Capybara.register_driver :chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
+  options.binary_location = "/usr/bin/google-chrome-stable"
   options.add_argument("--headless")
   options.add_argument("--disable-gpu")
   Capybara::Selenium::Driver.new(app, options, browser: :chrome)
