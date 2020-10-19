fea3b609b15e06c84c88019a7206c860cb624560
Trevor Moore <moore.trev@tamu.edu>
2020-10-19 17:05:52 -0500

    start google chrome111

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,20 +16,8 @@ Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: {
-      args: %w[headless enable-features=NetworkService,NetworkServiceInProcess]
-    }
-  )
-
-  Capybara::Selenium::Driver.new app,
-    browser: :chrome,
-    desired_capabilities: capabilities
-end
-
-Capybara.default_driver = :headless_chrome
-Capybara.javascript_driver = :headless_chrome
+Capybara.default_driver = :chrome
+Capybara.javascript_driver = :chrome
 
 Capybara.default_max_wait_time = 10
 
