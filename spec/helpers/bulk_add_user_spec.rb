9257cedefad08f2457dcd61d28971bb8d7491fde
zsteen13 <zsteen13@gmail.com>
2020-10-21 15:30:34 -0500

    fixed lots of things

--- spec/helpers/bulk_add_user_spec.rb
+++ spec/helpers/bulk_add_user_spec.rb
@@ -6,13 +6,13 @@ require 'csv'
 RSpec.describe BulkAddUsersHelper, type: :helper do
   it('should determine whether the number of columns is correct') do
     filename = Rails.root.join('spec', 'data', 'bulk_add_users_wrong.csv')
-    valid, row, col = BulkAddUsersHelper.check_num_cols(filename)
+    valid, row, col = BulkAddUsersHelper.check_num_columns(filename)
     expect(valid).to be false
     expect(row).to eq 3
     expect(col).to eq 3
 
     filename = Rails.root.join('spec', 'data', 'bulk_add_users_correct.csv')
-    valid, _row, _col = BulkAddUsersHelper.check_num_cols(filename)
+    valid, _row, _col = BulkAddUsersHelper.check_num_columns(filename)
     expect(valid).to be true
   end
   it('should be able to determine whether the input csv is value or not') do
@@ -39,7 +39,7 @@ RSpec.describe BulkAddUsersHelper, type: :helper do
   end
   it 'should be able handle a csv without optional values' do
     filename = Rails.root.join('spec', 'data', 'bulk_add_users_correct_2.csv')
-    users, valid = BulkAddUsersHelper.parseData(filename)
+    users, valid = BulkAddUsersHelper.parse_data(filename)
     user = users[0]
     expect(user.uin).to eq '958285839'
     expect(user.first_name).to eq 'trevor'
@@ -56,7 +56,7 @@ RSpec.describe BulkAddUsersHelper, type: :helper do
   end
   it 'should be able to handle a csv with some optional values' do
     filename = Rails.root.join('spec', 'data', 'bulk_add_users_correct_3.csv')
-    users, valid = BulkAddUsersHelper.parseData(filename)
+    users, valid = BulkAddUsersHelper.parse_data(filename)
     expect(valid).to be true
 
     expect(users[0].committee).to eq nil
