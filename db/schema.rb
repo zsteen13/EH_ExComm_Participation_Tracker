3022d77e524d0e2466dd1528b8b80a83725aae6f
David Acosta <David@Davids-Mac.local>
2020-11-02 18:58:36 -0600

    rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20_201_030_184_953) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension 'plpgsql'
+
   create_table 'activities', force: :cascade do |t|
     t.string 'name'
     t.string '_type'
