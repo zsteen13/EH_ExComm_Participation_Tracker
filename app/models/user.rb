f027c1f33d9644705098ee3ae2c299804aa562de
zsteen13 <zsteen13@gmail.com>
2020-11-03 18:57:09 -0600

    fixing capitalization issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,8 +46,8 @@ class User < ApplicationRecord
     args[:meeting_points] = args[:meeting_points].nil? ? 0 : args[:meeting_points]
     args[:event_points] = args[:event_points].nil? ? 0 : args[:event_points]
     args[:misc_points] = args[:misc_points].nil? ? 0 : args[:misc_points]
-    args[:first_name] = args[:first_name].capitalize()
-    args[:last_name] = args[:last_name].capitalize()
+    args[:first_name] = args[:first_name].capitalize unless args[:first_name].blank?
+    args[:last_name] = args[:last_name].capitalize unless args[:last_name].blank?
     args
   end
 
